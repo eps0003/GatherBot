96a4fd817586cdd360eb4cc696f43a1b5076bb23
epsilon <eps0003@users.noreply.github.com>
2020-10-17 12:48:29 +1100

    Revert do not disturb not allowed to add to queue

--- commands/add.js
+++ commands/add.js
@@ -12,7 +12,7 @@ module.exports = {
 		const status = message.member.presence.status;
 		if (match.isParticipating(message.member)) {
 			message.channel.send("You **cannot add** to the queue while **participating** in a match");
-		} else if (status !== "online") {
+		} else if (["idle", "offline"].includes(status)) {
 			const statusName = util.statusNames[status];
 			message.channel.send(`You **cannot add** to the queue while you are **${statusName}** on Discord`);
 		} else {
