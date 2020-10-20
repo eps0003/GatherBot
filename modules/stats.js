e7248919812472009602cbf1706380b71065d7eb
epsilon <eps0003@users.noreply.github.com>
2020-10-20 15:11:28 +1100

    Added leaderboard and season support

--- modules/stats.js
+++ modules/stats.js
@@ -1,6 +1,8 @@
 const SQLite = require("better-sqlite3");
 const sql = new SQLite('./stats.sqlite');
+const { client } = require("../index");
 const teams = require("./teams");
+const { table, getBorderCharacters } = require("table");
 
 var matchCompleted;
 var addPlayerMatch;
@@ -13,6 +15,7 @@ exports.init = () => {
 	sql.prepare(`
 		CREATE TABLE IF NOT EXISTS Matches (
 			match_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+			date TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
 			duration INTEGER,
 			map TEXT,
 			winner INTEGER,
@@ -28,9 +31,9 @@ exports.init = () => {
 			match_id INTEGER NOT NULL,
 			username TEXT NOT NULL,
 			team INTEGER NOT NULL,
-			kills INTEGER NOT NULL DEFAULT 0,
-			deaths INTEGER NOT NULL DEFAULT 0,
-			assists INTEGER NOT NULL DEFAULT 0,
+			kills INTEGER,
+			deaths INTEGER,
+			assists INTEGER,
 			PRIMARY KEY (match_id, username),
 			FOREIGN KEY(match_id) REFERENCES Matches(match_id)
 		);
@@ -43,7 +46,7 @@ exports.init = () => {
 			username,
 			SUM(team = winner) as wins,
 			SUM(team != winner) as losses,
-			CAST(SUM(team = winner) AS FLOAT) / NULLIF(COUNT(1), 0) as winrate,
+			CAST(SUM(team = winner) AS FLOAT) / COUNT(1) as winrate,
 			SUM(kills) AS kills,
 			SUM(deaths) AS deaths,
 			SUM(assists) AS assists,
@@ -64,20 +67,24 @@ exports.init = () => {
 		NATURAL JOIN Matches
 	`;
 
+	const seasonCheck = process.env.SEASON_START ? `date >= DATETIME('${process.env.SEASON_START}')` : "1";
+
 	matchCompleted = sql.prepare("INSERT INTO Matches (duration, map, winner, blue_tickets, red_tickets, win_condition) VALUES (@duration, @map, @winner, @blueTickets, @redTickets, @cause)");
 	addPlayerMatch = sql.prepare("INSERT INTO PlayerMatches VALUES (@matchID, @username, @team, @kills, @deaths, @assists)");
 	getLastID = sql.prepare("SELECT last_insert_rowid() AS 'id'");
-	getLeaderboard = sql.prepare(`${statsQuery} GROUP by username ORDER BY winrate DESC, wins DESC LIMIT ?`);
-	getStats = sql.prepare(`${statsQuery} WHERE username LIKE ? GROUP by username`);
+	getLeaderboard = sql.prepare(`${statsQuery} WHERE ${seasonCheck} GROUP by username ORDER BY winrate DESC, wins DESC LIMIT ?`);
+	getStats = sql.prepare(`${statsQuery} WHERE username LIKE ? AND ${seasonCheck} GROUP by username`);
+
+	this.updateLeaderboardMessage();
 };
 
 exports.getLeaderboard = (max = 20) => {
 	return getLeaderboard.all(max);
-}
+};
 
 exports.getStats = (username) => {
 	return getStats.get(username);
-}
+};
 
 exports.saveMatch = (cause, winner, duration, map, blueTickets, redTickets, playerStats) => {
 	matchCompleted.run({ duration, map, winner, blueTickets, redTickets, cause });
@@ -104,4 +111,51 @@ exports.saveMatch = (cause, winner, duration, map, blueTickets, redTickets, play
 			...playerStats[player.username]
 		});
 	}
+
+	this.updateLeaderboardMessage();
+};
+
+exports.getLeaderboardText = (max = 20) => {
+	const data = [["[Username]", "[Matches]", " [Wins] ", "[Losses]", "[Win %]", "[KDR]"]];
+
+	const leaderboard = this.getLeaderboard(max);
+	for (const x of leaderboard) {
+		const formattedWinrate = Math.floor(x.winrate * 100).toFixed(2);
+		data.push([x.username, x.playcount, x.wins, x.losses, `${formattedWinrate}%`, x.kdr.toFixed(2)]);
+	}
+
+	const config = {
+		columns: {
+			0: { width: 20, alignment: "center" },
+			1: { alignment: "right" },
+			2: { alignment: "right" },
+			3: { alignment: "right" },
+			4: { alignment: "right" },
+			5: { alignment: "right" },
+		},
+		drawHorizontalLine: (index, size) => {
+			return index === 0 || index === 1 || index === size;
+		},
+	};
+
+	return "```ini\n" + table(data, config) + "```";
+};
+
+exports.updateLeaderboardMessage = () => {
+	//disable leaderboard if an env variable is omitted
+	if (!process.env.LEADERBOARD_CHANNEL || !process.env.LEADERBOARD_MESSAGE) return;
+
+	//get channel
+	const channel = client.channels.cache.get(process.env.LEADERBOARD_CHANNEL);
+	if (!channel) {
+		console.warn("Unable to find leaderboard channel");
+		return;
+	}
+
+	//fetch message
+	channel.messages.fetch(process.env.LEADERBOARD_MESSAGE).then((message) => {
+		message.edit(this.getLeaderboardText());
+	}).catch((err) => {
+		console.warn("Unable to find leaderboard message");
+	});
 };
